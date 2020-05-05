b6a92eeeac504f80c5b429e4c20b86694a7d2634
kevincodexgithub <kevintauro95@gmail.com>
2020-05-04 23:09:20 -0500

    Hola mundo

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Hola Mundo</title>
+	<title>Hello word</title>
 
 	<style type="text/css">
 
